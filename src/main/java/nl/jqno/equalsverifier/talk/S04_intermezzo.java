941316dbf4cf08bddfd5fc46efed73710027edf9
Jan Ouwens <jan.ouwens@gmail.com>
2013-08-11 15:27:03 +0200

    Slide 4: Intermezzo

---
+++ src/main/java/nl/jqno/equalsverifier/talk/S04_intermezzo.java
@@ -0,0 +1,75 @@
+package nl.jqno.equalsverifier.talk;
+
+public class S04_intermezzo {
+
+
+	/*
+	 * Why did EqualsVerifier say that "equals is not final"?
+	 * 
+	 */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * First, we need to know the equals contract:
+	 * 
+	 * - Reflexivity       x == x
+	 * 
+	 * - Symmetry          if x == y, then y == x
+	 * 
+	 * - Transitivity      if x == y and y == z, then x == z
+	 * 
+	 * - Consistency       if x == y now, then x == y always
+	 * 
+	 * - Non-nullity       x == null is never true
+	 * 
+	 */
+	
+	
+	
+	
+	
+	
+	/*
+	 * (Our mutable Point breaks consistency.)
+	 * 
+	 */
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
