a4da310e55a4a8a353dff0426e0a05d2220a9339
Jan Ouwens <jan.ouwens@gmail.com>
2013-08-11 13:16:32 +0200

    Slide 2: hashCode

---
+++ src/main/java/nl/jqno/equalsverifier/talk/S02_hashcode.java
@@ -0,0 +1,126 @@
+package nl.jqno.equalsverifier.talk;
+
+import static junit.framework.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import nl.jqno.equalsverifier.talk.helper.EqualsVerifier;
+
+import org.junit.Test;
+
+public class S02_hashcode {
+	
+	
+	/*
+	 * Let's start with Point again.
+	 * This time, it has the correct equals signature.
+	 */
+	public class Point {
+		private int x;
+		private int y;
+		
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (!(obj instanceof Point)) {
+				return false;
+			}
+			Point other = (Point)obj;
+			return x == other.x && y == other.y;
+		}
+	}
+	
+	private Point p1 = new Point(0, 1);
+	private Point p2 = new Point(0, 1);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * Why do we care about hashCode?
+	 * 
+	 * Well...
+	 */
+	@Test
+	public void object_can_be_found_in_a_hash_based_collection() {
+		Set<Point> set = new HashSet<Point>();
+		set.add(p1);
+		
+		assertTrue(set.contains(p2));  //  <-- Fails... PROBABLY.
+	}
+	
+	@Test
+	public void equalsverifier() {
+		EqualsVerifier.forClass(Point.class)
+				.verify();
+	}
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * The hashCode contract:
+	 * 
+	 * - Always returns the same value (per execution);
+	 * - If two objects are equal, they must have the same hashCode.
+	 */
+
+
+	/*
+		@Override
+		public int hashCode() {
+			return 42;  //  <-- Fulfills the contract! Oh yeah.
+		}
+	 */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * Let's write a hashCode with a proper distribution.
+	 */
+	
+
+	/*
+		@Override
+		public int hashCode() {
+			int result = 1;
+			result = (31 * result) + x;
+			result = (31 * result) + y;
+			return result;
+		}
+	 */
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/nl/jqno/equalsverifier/talk/helper/EqualsVerifier.java
@@ -0,0 +1,15 @@
+package nl.jqno.equalsverifier.talk.helper;
+
+import nl.jqno.equalsverifier.Warning;
+
+/*
+ * OK, OK, I'm cheating a bit here:
+ * I don't want the inheritance-related warnings to show up just yet.
+ * 
+ * No need to show this to the audience. :P
+ */
+public class EqualsVerifier {
+	public static <T> nl.jqno.equalsverifier.EqualsVerifier<T> forClass(Class<T> type) {
+		return nl.jqno.equalsverifier.EqualsVerifier.forClass(type).suppress(Warning.STRICT_INHERITANCE);
+	}
+}
