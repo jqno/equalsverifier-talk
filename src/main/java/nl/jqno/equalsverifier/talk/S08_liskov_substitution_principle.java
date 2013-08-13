278a33beb65c1ff90521ee21f79e51a37986880a
Jan Ouwens <jan.ouwens@gmail.com>
2013-08-13 17:23:44 +0200

    Slide 8: Liskov Substitution Principle

---
+++ src/main/java/nl/jqno/equalsverifier/talk/S08_liskov_substitution_principle.java
@@ -0,0 +1,125 @@
+package nl.jqno.equalsverifier.talk;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+import org.junit.Test;
+
+public class S08_liskov_substitution_principle {
+	
+	/*
+	 * Liskov Substitution Principle:
+	 * 
+	 * - if S is a subtype of T,
+	 * - then objects of type T may be replaced with objects of type S
+	 * - and no-one would notice.
+	 */
+
+
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * This is the same Point as in the previous slide.
+	 */
+	public class Point {
+		private final int x;
+		private final int y;
+		
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int hashCode() {
+			return 31 * (31 + x) + y;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			// Use getClass instead of instanceof
+			if (obj == null || getClass() != obj.getClass()) {
+				return false;
+			}
+			Point other = (Point)obj;
+			return x == other.x && y == other.y;
+		}
+	}		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * Now let's pretend that we're Hibernate.
+	 */
+	public class Point$$Proxy extends Point {
+		public Point$$Proxy(int x, int y) {
+			super(x, y);
+		}
+	}
+	
+	@Test
+	public void equalsverifier_on_proxy() {
+		EqualsVerifier.forClass(Point$$Proxy.class)
+				.verify();
+	}
+	
+	
+
+	
+	
+
+	
+	
+	
+	
+	
+	/*
+	 * I recommend using 'instanceof' instead of 'getClass'.
+	 */
+	
+	
+	/*
+		@Override
+		public boolean equals(Object obj) {
+			if (!(obj instanceof Point)) {
+				return false;
+			}
+			Point other = (Point)obj;
+			return x == other.x && y == other.y;
+		}
+	 */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
